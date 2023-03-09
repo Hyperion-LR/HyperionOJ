4ab94039247982721164b98019d129a28938035d
Hyperion-LR <Hyperion_LR@foxmail.com>
2023-03-09 12:08:12 +0800

    fix:部署相关

--- HyperionOJ-Cloud/bin/build.sh
+++ HyperionOJ-Cloud/bin/build.sh
@@ -11,7 +11,7 @@ mvn compile
 
 mvn install
 
-rm -rf ~/HyperionOJ-WEB/release/
+rm -rf ~/HyperionOJ-WEB/release/*
 
 mv ~/hyperion-oj/HyperionOJ-Cloud/hyperionoj-web/target/hyperionoj-web-1.0.0.jar ~/HyperionOJ-WEB/release/
 
