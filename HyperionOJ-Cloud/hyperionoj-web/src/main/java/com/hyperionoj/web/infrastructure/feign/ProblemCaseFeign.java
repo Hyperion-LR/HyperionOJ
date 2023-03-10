4149e89cbb8ffa6137011a26e1d86085098e4411
Hyperion-LR <Hyperion_LR@foxmail.com>
2023-03-10 20:32:10 +0800

    fix: 修复feign功能

--- HyperionOJ-Cloud/hyperionoj-web/src/main/java/com/hyperionoj/web/infrastructure/feign/ProblemCaseFeign.java
+++ HyperionOJ-Cloud/hyperionoj-web/src/main/java/com/hyperionoj/web/infrastructure/feign/ProblemCaseFeign.java
@@ -1,6 +1,7 @@
 package com.hyperionoj.web.infrastructure.feign;
 
 import com.hyperionoj.web.presentation.vo.Result;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 public interface ProblemCaseFeign {
 
     @PostMapping(value = "/problem/case/{problemId}")
+    @Headers({"Content-Type: multipart/form-data"})
     Result pushProblemCase(@PathVariable("problemId") Long problemId,
                            @RequestParam("caseInList") MultipartFile[] inMultipartFiles,
                            @RequestParam("caseOutList") MultipartFile[] outMultipartFiles);
