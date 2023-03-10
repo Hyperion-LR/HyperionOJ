f525a1b5c9937a322d7c004bf725d16aa4eb3087
Hyperion-LR <Hyperion_LR@foxmail.com>
2023-03-10 20:06:52 +0800

    add: web提交测试点

--- HyperionOJ-Cloud/hyperionoj-web/src/main/java/com/hyperionoj/web/infrastructure/feign/ProblemCaseFeign.java
+++ HyperionOJ-Cloud/hyperionoj-web/src/main/java/com/hyperionoj/web/infrastructure/feign/ProblemCaseFeign.java
@@ -1,15 +1,12 @@
 package com.hyperionoj.web.infrastructure.feign;
 
-import com.hyperionoj.web.infrastructure.config.FeignConfig;
 import com.hyperionoj.web.presentation.vo.Result;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-@Service
-@FeignClient(value = "hyperionoj-judge", configuration = FeignConfig.class)
+@FeignClient(value = "hyperionoj-judge")
 public interface ProblemCaseFeign {
 
     @PostMapping(value = "/problem/case/{problemId}")
